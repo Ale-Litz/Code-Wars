31c51c5671ebe04c7461830481e6bd7741b68186
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2022-06-15 11:21:57 -0300

    Update and rename Regex Replace to Regex Replace.sql

--- SQL/7 kyu/Regex Replace.sql
+++ SQL/7 kyu/Regex Replace.sql
@@ -1,7 +1,9 @@
+--https://www.codewars.com/kata/5942b066db68b6f35f000084
+
 select project, commits, contributors, translate(address,'1234567890','!!!!!!!!!!') address
 from repositories
 
---or--
+----------
 
 select project, commits, contributors, regexp_replace(address,'[0-9]','!','g') address
 from repositories
