d1d624623bc5c22c496eef03446feaac4f4154d1
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2022-06-15 14:07:38 -0300

    Update Penultimate.py

--- Python/7 kyu/Penultimate.py
+++ Python/7 kyu/Penultimate.py
@@ -1,4 +1,4 @@
-#https://www.codewars.com/kata/54162d1333c02486a700011d/train/python
+[Penultimate](https://www.codewars.com/kata/54162d1333c02486a700011d/train/python)
 
 def penultimate(a):
     ultimo = a[-2]
