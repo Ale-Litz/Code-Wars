ceb064e8a04d83bca137b80778985dbff907e3b3
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2022-06-16 21:40:55 -0300

    Create List Filtering.py

---
+++ Python/7 kyu/List Filtering.py
@@ -0,0 +1,13 @@
+#https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
+
+def filter_list(l):
+    result = []
+    for i in l:
+        if type(i) == int:
+            result.append(i)
+    return result
+  
+  ----------
+  
+  def filter_list(l):
+  return [i for i in l if not isinstance(i, str)]
