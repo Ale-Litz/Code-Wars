0de4f058464e75668f745bce9c314fdd5efe745d
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2022-06-22 14:03:00 -0300

    Update Thinking & Testing: A and B?.py

--- Python/7 kyu/Thinking & Testing: A and B?.py
+++ Python/7 kyu/Thinking & Testing: A and B?.py
@@ -1,8 +1,4 @@
 #https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/python
 
 def testit (a, b):
-    if (a == 0) or ((b/2) == a):
-        x = a + b
-    else:
-        x = a * b
-    return x
+    return a | b
